a04f8ff4e09d05e679f05771f532e2f82b1453d1
Jorge Cortes <jcortes@belatrixsf.com>
2017-09-11 08:36:23 -0500

    Version from office

--- test.hs
+++ test.hs
@@ -1,11 +1,42 @@
 double x = x + x
 
-quadruple x = double (double x)
+quadruple
+  x =
+    double (double x)
 
-factorial n = product [1..n]
+factorial
+  n =
+    product [1..n]
 
-average ns = sum ns `div` length ns
+average
+  ns =
+    sum ns `div` length ns
 
-doubleSmallNumber x = if x > 100
-                       then x
-                       else x * 2
+doubleSmallNumber
+  x =
+    if x > 100
+    then x
+    else x * 2
+
+-- halve xs =
+--   splitAt (length xs `div` 2) xs
+-- halve xs =
+--   (take (n `div` 2) xs, drop (n `div` 2) xs)
+--   where n = length xs
+-- halve xs = splitAt (div (length xs) 2) xs
+halve xs = (take n xs, drop n xs)
+  where n = length xs `div` 2
+
+-- safetail (_:xs)
+--   | null xs = []
+--   | otherwise = tail xs
+
+-- mult x y z = \ x -> (\ y -> (\ z -> x * y * z))
+-- mult = \ x -> (x * \ y -> (\ z -> x * y * z))
+mult = \ x -> (\ y -> (\ z -> x * y * z))
+
+fix :: (a -> a) -> a
+-- cb a = a
+-- f cb = cb
+fix f = let x = f x in x
+
