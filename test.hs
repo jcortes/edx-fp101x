938621674e902b7226668d39e65b62631d501082
Jorge Cortes <jcortes@belatrixsf.com>
2017-09-13 15:28:15 -0500

    new exercises

--- test.hs
+++ test.hs
@@ -2,23 +2,16 @@ import Data.Char
 
 double x = x + x
 
-quadruple
-  x =
-    double (double x)
+quadruple x = double (double x)
 
-factorial
-  n =
-    product [1..n]
+factorial n = product [1..n]
 
-average
-  ns =
-    sum ns `div` length ns
+average ns = sum ns `div` length ns
 
-doubleSmallNumber
-  x =
-    if x > 100
-    then x
-    else x * 2
+doubleSmallNumber x =
+  if x > 100
+  then x
+  else x * 2
 
 halve :: Ord a => [a] -> ([a], [a])
 halve xs = splitAt (length xs `div` 2) xs
@@ -143,4 +136,73 @@ msort xs = merge (msort ys) (msort zs)
 -- [1,2,3,4,5,6]
 
 myAll :: (a -> Bool) -> [a] -> Bool
-myAll p xs = and (map p xs)
+-- myAll p xs = and (map p xs) -- Good
+-- myAll p xs = map p (and xs) -- Wrong
+-- myAll p = and . map p -- Good
+-- myAll p = not . any (not . p) -- Good
+-- myAll p = map p . and -- Wrong
+-- myAll p xs = foldl (&&) True (map p xs) -- Good
+-- myAll p xs = foldr (&&) False (map p xs) -- Wrong
+myAll p = foldr (&&) True . map p -- Good
+
+myAny :: (a -> Bool) -> [a] -> Bool
+-- myAny p = map p . or -- Wrong
+-- myAny p = or . map p -- Good
+-- myAny p xs = length (filter p xs) > 0 -- Good
+-- myAny p = not . null . dropWhile (not . p) -- Good
+-- myAny p = null . filter p -- Wrong
+-- myAny p xs = not (all (\x -> not (p x)) xs) -- Good
+myAny p xs = foldr (\x acc -> (p x) || acc) False xs -- Good
+-- myAny p xs = foldr (||) True (map p xs) -- Wrong
+
+myTakeWhile :: (a -> Bool) -> [a] -> [a]
+myTakeWhile _ [] = []
+myTakeWhile p (x:xs)
+  | p x = x : myTakeWhile p xs
+  | otherwise = []
+
+myDropWhile :: (a -> Bool) -> [a] -> [a]
+myDropWhile _ [] = []
+myDropWhile p (x:xs)
+  | p x = myDropWhile p xs
+  | otherwise = x : xs
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldl (\xs x -> xs ++ [f x]) []
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter p = foldr (\x xs -> if p x then x : xs else xs) []
+
+dec2int :: [Integer] -> Integer
+dec2int = foldl (\x y -> 10 * x + y) 0
+
+curry :: ((a, b) -> c) -> a -> b -> c
+curry f = \ x y -> f (x, y)
+
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f = \ (x, y) -> f x y
+
+unfold :: (b -> Bool) -> (b -> a) -> (b -> b) -> b -> [a]
+unfold p h t x
+  | p x = []
+  | otherwise = h x : unfold p h t (t x)
+
+type Bit = Int
+int2bin :: Int -> [Bit]
+-- int2bin 0 = []
+-- int2bin n = n `mod` 2 : int2bin (n `div` 2)
+int2bin = unfold (== 0) (`mod` 2) (`div` 2)
+
+chop8 :: [Bit] -> [[Bit]]
+-- chop8 [] = []
+-- chop8 bits = take 8 bits : chop8 (drop 8 bits)
+-- chop8 = unfold [] (drop 8) (take 8) -- Wrong
+chop8 = unfold null (take 8) (drop 8)
+
+myOtherMap :: (a -> b) -> [a] -> [b]
+myOtherMap f = unfold null (f . head) tail
+
+myIterate :: (a -> a) -> a -> [a]
+myIterate f = unfold (const False) id f
+
+-- Ex 15
