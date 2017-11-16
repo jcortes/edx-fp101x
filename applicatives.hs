82d7dbbfe2ad98d8d63738867625cba44ba82b76
Jorge Cortes <jcortes@belatrixsf.com>
2017-11-16 10:34:55 -0500

    updated

--- applicatives.hs
+++ applicatives.hs
@@ -1,6 +1,6 @@
--- module Book where
+module Book where
 
-import Prelude hiding (Maybe, Just, Nothing)
+import Prelude hiding (Maybe, Just, Nothing, Applicative, pure, (<*>), (<$>), sequenceA)
 
 data Maybe a = Nothing | Just a
   deriving Show
@@ -14,7 +14,7 @@ class Functor f => Applicative f where
   pure :: a -> f a
   (<*>) :: f (a -> b) -> f a -> f b
 
-instance Prelude.Applicative Maybe where
+instance Applicative Maybe where
   -- pure :: a -> Maybe a
   pure = Just
   -- (<*>) :: Maybe (a -> b) -> Maybe a -> Maybe b
@@ -24,7 +24,46 @@ instance Prelude.Applicative Maybe where
 data CMaybe a = CNothing | CJust Int a
   deriving (Show)
 
-instance Functor CMaybe where  
+instance Functor CMaybe where
   fmap f CNothing = CNothing
   fmap f (CJust counter x) = CJust (counter+1) (f x)
 
+instance Applicative [] where
+  pure x = [x]  
+  fs <*> xs = [f x | f <- fs, x <- xs]
+
+(<$>) :: (Functor f) => (a -> b) -> f a -> f b  
+f <$> x = fmap f x
+
+instance Applicative IO where  
+  pure = return
+  a <*> b = do
+    f <- a
+    x <- b
+    return (f x)
+
+myAction :: IO String
+myAction = (++) <$> getLine <*> getLine
+
+main = do
+  a <- (++) <$> getLine <*> getLine
+  putStrLn $ "The two lines concatenated turn out to be: " ++ a
+
+instance Applicative ((->) r) where
+  pure x = (\_ -> x)
+  f <*> g = \x -> f x (g x)
+
+-- instance Applicative ZipList where
+--   pure x = ZipList (repeat x)
+--   ZipList fs <*> ZipList xs = ZipList (zipWith (\f x -> f x) fs xs)
+
+-- getChars :: Int -> IO String
+-- getChars 0 = return []
+-- getChars n = pure (:) <*> getChar <*> getChars (n-1)
+
+sequenceA :: Applicative f => [f a] -> f [a]
+sequenceA [] = pure []
+sequenceA (x:xs) = pure (:) <*> x <*> sequenceA xs
+
+getChars :: Int -> IO String
+getChars n = sequenceA (replicate n getChar)
