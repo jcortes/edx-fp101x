91a744163ff3718d6a6ef0c103f32af39c51cb5a
Jorge Cortes <jcortes@belatrixsf.com>
2018-07-30 17:01:47 -0500

    minor modifications

--- card-validation.hs
+++ card-validation.hs
@@ -2,9 +2,33 @@ toDigits :: Integer -> [Integer]
 toDigits 0 = [0]
 toDigits n = toDigits (n `div` 10) ++ [n `mod` 10]
 
+-- toDigits 1234
+-- (++) (toDigits (1234 `div` 10))  [1234 `mod` 10]
+-- (++) ((++) (toDigits (123 `div` 10)) [123 `mod` 10]) [1234 `mod` 10]
+-- (++) ((++) ((++) (toDigits (12 `div` 10)) [12 `mod` 10]) [123 `mod` 10]) [1234 `mod` 10]
+-- (++) ((++) ((++) ((++) (toDigits (1 `div` 10)) [1 `mod` 10]) [12 `mod` 10]) [123 `mod` 10]) [1234 `mod` 10]
+-- (++) ((++) ((++) ((++) (toDigits (0)) [1]) [2]) [3]) [4]
+-- (++) ((++) ((++) ((++) [0] [1]) [2]) [3]) [4]
+-- (++) ((++) ((++) [0,1] [2]) [3]) [4]
+-- (++) ((++) [0,1,2] [3]) [4]
+-- (++) [0,1,2,3] [4]
+-- [0,1,2,3,4]
+
 toDigitsReverse :: Integer -> [Integer]
 toDigitsReverse 0 = [0]
 toDigitsReverse n = n `mod` 10 : toDigitsReverse (n `div` 10)
+
+-- toDigitsReverse 1234
+-- (:) (1234 `mod` 10) (toDigitsReverse (1234 `div` 10))
+-- (:) (1234 `mod` 10) ((:) (123 `mod` 10) (toDigitsReverse (123 `div` 10)))
+-- (:) (1234 `mod` 10) ((:) (123 `mod` 10) ((:) (12 `mod` 10) (toDigitsReverse (12 `div` 10))))
+-- (:) (1234 `mod` 10) ((:) (123 `mod` 10) ((:) (12 `mod` 10) ((:) (1 `mod` 10) (toDigitsReverse (1 `div` 10)))))
+-- (:) (4) ((:) (3) ((:) (2) ((:) (1) (toDigitsReverse (0)))))
+-- (:) (4) ((:) (3) ((:) (2) ((:) (1) [0])))
+-- (:) (4) ((:) (3) ((:) (2) [1,0]))
+-- (:) (4) ((:) (3) [2,1,0])
+-- (:) (4) [3,2,1,0]
+-- [4,3,2,1,0]
 
 doubleSecond :: [Integer] -> [Integer]
 doubleSecond xs = [if even idx then y + y else y | (y,idx) <- ys]
