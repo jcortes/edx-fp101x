a1cb42cfbaf2645f06e03fecf2432fd280e88a70
Jorge Cortes <jcortes@belatrixsf.com>
2018-09-21 16:47:15 -0500

    modifications

--- parsers.hs
+++ parsers.hs
@@ -18,18 +18,4 @@ parse p inp = p inp
 p +++ q = \inp -> case p inp of
   [] -> parse q inp
   [(v,out)] -> [(v,out)]
-  
--- p' :: Parser ([(Char, String)], [(Char, String)])
--- p' :: Parser (Char, Char)
-p' = do
-      [(x:xs)] <- item
-      -- [(y:ys)] <- item xs
-      return' ('A','B')
 
--- sat :: (Char -> Bool) -> Parser Char
--- sat p = do 
---   x <- item
---   if p x then
---     return' x
---   else
---     failure
